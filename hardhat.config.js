ef41f8154022d2efee5ff27d378e676f15350597
Cameron Bechtloff <cbechtloff@gmail.com>
2022-12-28 09:36:31 -0500

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,12 @@ const GORELI_RPC_URL = process.env.GORELI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const CMC_API_KEY = process.env.CMC_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: {
-		compilers: [{ version: "0.8.9" }, { version: "0.8.4" }],
+		compilers: [{ version: "0.8.9" }, { version: "0.8.4" }, { version: "0,8.0" }],
 	},
 	defaultNetwork: "hardhat",
 	networks: {
@@ -25,6 +26,9 @@ module.exports = {
 		hardhat: {
 			chainId: 31337,
 			blockConfirmations: 1,
+			// forking: {
+			// 	url: MAINNET_RPC_URL,
+			// },
 		},
 	},
 	etherscan: {
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,10 +1,21 @@
 const networkConfig = {
 	5: {
 		name: "goerli",
-		ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+		subscriptionId: "7753",
+		vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+		entranceFee: ethers.utils.parseEther("0.01"),
+		gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+		callbackGasLimit: "500000",
+		interval: "30",
+		mintFee: "10000000000000000",
 	},
 	31337: {
-		name: "localhost",
+		name: "hardhat",
+		entranceFee: ethers.utils.parseEther("0.01"),
+		gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+		callbackGasLimit: "500000",
+		interval: "30",
+		mintFee: "10000000000000000",
 	},
 };
 
